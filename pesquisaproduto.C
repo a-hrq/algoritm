e981a6672f57b3e1cb7a6701569e44df282ec3da
Amadeus Henrique <119773339+a-hrq@users.noreply.github.com>
2023-02-04 15:41:01 -0300

    Update pesquisaproduto.C

--- pesquisaproduto.C
+++ pesquisaproduto.C
@@ -12,28 +12,36 @@ printf("Digite o numero de entrevistados: ");
 scanf("%d", &n);
 v1 = (char*) malloc(n*sizeof(char));
 v2 = (char*) malloc(n*sizeof(char));
+
+if(v1 == NULL || v2 == NULL){
+printf("Espaço insuficiente!");
+exit(1);
+}
+
 fflush(stdin);
 fg = mng = 0;
 
 for(i = 0; i < n; i++){
-printf("Digite seu sexo (m (macho) ou f (femea)): ");
+printf("Digite o sexo do %d° entrevistado (responda com m (macho) ou f (femea)): ", i+1);
 scanf("%c", &v1[i]);
 fflush(stdin);
-printf("Voce gostou do produto (responda com s(sim ou n(nao)): ");
+printf("Ele(a) gostou do produto (responda com s(sim ou n(nao))? ");
 scanf("%c", &v2[i]);
 fflush(stdin);
- if(v1[i] == 'f'){
-    if(v2[i] == 's'){
-        fg++;
+ if(v1[i] == 'f' || v1[i] == 'F'){
+    if(v2[i] == 's' || v2[i] == 'S'){
+    fg++;
     }
  } else{
-   if(v2[i] == 'n'){
+   if(v2[i] == 'n' || v2[i] == 'N'){
     mng++;
    }
  }
 }
 printf("A porcrentagem de mulheres que gostaram eh igual a: %f\n", fg*100/(float)n);
 printf("A porcrentagem de homens que n gostaram eh igual a: %f", mng*100/(float)n);
+free(v1);
+free(v2);
 
 return 0;
 }
